fd2f9ffc82f2a18ed6438784b30282db1b9e944a
xSasuke11x <xSasuke11x@gmail.com>
2018-01-17 01:32:38 -0500

    Added Remodel and Smithy.

--- src/DominionPackage/CardEffects.java
+++ src/DominionPackage/CardEffects.java
@@ -42,7 +42,8 @@ public class CardEffects {
 					
 					numDiscard++;
 				} else {
-					System.out.println("That is not a valid choice");
+					System.out.println("That is not a valid choice. Choose a number between between 0 (discard none) and " + player1.getCardsInHand().size());
+					choice = scan.nextInt();
 				}
 			}
 			
@@ -86,7 +87,8 @@ public class CardEffects {
 					// Add the card that was removed from the hand to the trash pile
 					kingdoms.trash.add(card);
 				} else {
-					System.out.println("That is not a valid choice");
+					System.out.println("That is not a valid choice. Choose a number between 0 (trash none) and " + player1.getCardsInHand().size());
+					choice = scan.nextInt();
 				}
 			}
 		}
@@ -141,7 +143,8 @@ public class CardEffects {
 					
 					continueLooping = false;
 				} else {
-					System.out.println("That is not a valid choice");
+					System.out.println("That is not a valid choice. Choose a number between 0 (cancel effect) and " + player1.getDiscardPile().size());
+					choice = scan.nextInt();
 				}
 			}
 		}
@@ -188,7 +191,7 @@ public class CardEffects {
 			Card card2 = player1.getDiscardPile().get(0);
 			
 			// If the top card of the discard pile is an action card...
-			if (card2.getType1().equals("Action")) {
+			if ("Action".equals(card2.getType1())) {
 				System.out.println("The card you just discard: ");
 				System.out.println(player1.getDiscardPile().get(0));
 				System.out.println(" is an Action card. Would you like to play it? Press [y] for yes or [n] for no.");
@@ -263,6 +266,7 @@ public class CardEffects {
 					kingdoms.removeCardFromSupplyList(kingdom.get(0));
 				} else {
 					System.out.println("Invalid number. Please press a number between 1 and " + fourCostKingdoms.size() + " to gain the card");
+					choice = scan.nextLine();
 				}
 			}
 		}
@@ -325,6 +329,7 @@ public class CardEffects {
 						}
 					} else {
 						System.out.println("Invalid choice. Press a number between 1 and " + vicCardsInHand.size() + " to place the card back onto the deck.");
+						choice = scan.nextLine();
 					}
 				}
 			}
@@ -409,7 +414,9 @@ public class CardEffects {
 						// Remove the selected card from the player's hand
 						player2.removeCardFromHand(card);
 					} else {
-						System.out.println("Invalid choice. Press a number between 1 and " + player2.getCardsInHand().size() + " to place the card back onto the deck.");
+						System.out.println("Invalid choice. Press a number between 1 and " + player2.getCardsInHand().size() 
+								+ " to place the card back onto the deck.");
+						choice = scan.nextLine();
 					}
 				}
 			}
@@ -507,19 +514,135 @@ public class CardEffects {
 							
 							// Remove a card from the player's hand
 							player1.removeCardFromHand(card);
-							
-							// Go back to original loop to keep count of cards to discard and to re-print prompt
-							break;
 						} else {
 							// Invalid choice
 							System.out.println("That is not a valid number. Please choose a number between 1 and " + player1.getCardsInHand().size() 
 								+ " to discard that card.");
-							break;
+							choice = scan.nextLine();
 						}
 					}
 				} else {
 					break;
 				}
+			}
+		}
+	}
+	
+	public void Remodel(Kingdom kingdoms, int playerTurnCounter, Player player1, Player player2, Player player3, Player player4) {
+		if (playerTurnCounter == 1) {
+			
+			// Effect
+			System.out.println("Trash a card in your hand. Gain a card costing up to +2 Coins more than it.");
+			
+			if (player1.getCardsInHand().size() > 0) {
+				System.out.println("Choose a number between 1 and " + player1.getCardsInHand().size() + " to trash.");
+				Scanner scan = new Scanner(System.in);
+				int choice;
+				boolean cardDiscarded = false;
+				
+				while (cardDiscarded == false) {
+					choice = scan.nextInt();
+					
+					// Print out the cards with the value
+					System.out.println("Cards in hand:");
+					for (int i = 0; i < player1.getCardsInHand().size(); i++) {
+						
+						System.out.println((i + 1) + " = " + player1.getCardsInHand().get(i).getName());
+					}
+					
+					// Remove the chosen card from the hand and add it to the discard pile
+					if (choice > 0 && choice <= player1.getCardsInHand().size()) {
+						
+						// Get the selected card
+						Card card = player1.getCardsInHand().get(choice - 1);
+						
+						// Remove the card from the hand
+						player1.removeCardFromHand(card);
+						
+						// Add the card that was removed from the hand to the trash pile
+						kingdoms.trash.add(card);
+						
+						// Set the boolean to indicate that the player has trashed a card.
+						cardDiscarded = true;
+						
+						List<List<Card>> twoMoreCostKingdoms = new ArrayList<List<Card>>();
+						
+						// Add all kingdoms to the twoMoreCostKingdoms list that cost at most 2 more than the discarded card
+						for (List<Card> kingdom : kingdoms.supplyList) {
+							try {
+								if (Integer.parseInt(kingdom.get(0).getCost()) <= (Integer.parseInt(card.getCost() + 2))) {
+									twoMoreCostKingdoms.add(kingdom);
+								}
+							} catch (NumberFormatException ex) {
+								// Catch is for formality to catch out integer parsing errors. The Try block still parses all 4-cost kingdoms correctly.
+							}
+						}
+						
+						// Print out the twoMoreCostKingdoms list
+						int j = 1;
+						System.out.println("The following are the kingdoms that cost up to two more than your discarded card:");
+						for (List<Card> kingdom : twoMoreCostKingdoms) {
+							System.out.println(j + " = " + kingdom);
+							j++;
+						}
+						
+						// Let the player choose a kingdom
+						System.out.println("Press a number between 1 and " + twoMoreCostKingdoms.size() + " to gain the card");
+						Scanner scan2 = new Scanner(System.in);
+						String choice2;
+						while (scan.hasNext()) {
+							choice2 = scan2.nextLine();
+							
+							// Choices must be a number between 1 and the size of the twoMoreCostKingdoms list
+							if (Integer.parseInt(choice2) > 0 && Integer.parseInt(choice2) <= twoMoreCostKingdoms.size()) {
+								List<Card> kingdom = new ArrayList<Card>();
+								kingdom = twoMoreCostKingdoms.get(Integer.parseInt(choice2) - 1);
+								
+								// Add the card from the kingdom to the discard pile
+								player1.addCardToDiscardPile(kingdom.get(0));
+								
+								// Remove the card from the kingdom
+								kingdoms.removeCardFromSupplyList(kingdom.get(0));
+							} else {
+								System.out.println("Invalid number. Please press a number between 1 and " + twoMoreCostKingdoms.size() + " to gain the card");
+								choice2 = scan2.nextLine();
+							}
+						}
+					} else {
+						System.out.println("That is not a valid choice. Choose a number between 1 and " + player1.getCardsInHand().size() + " to trash.");
+						choice = scan.nextInt();
+					}
+				}
+			} else {
+				System.out.println("You do not have enough cards in your hand to trash.");
+			}
+		}
+	}
+	
+	public void Smithy(Kingdom kingdoms, int playerTurnCounter, Player player1, Player player2, Player player3, Player player4) {
+		if (playerTurnCounter == 1) {
+			
+			// +3 Cards
+			for (int i = 0; i < 3; i++) {
+				player1.drawCard(player1.getDeck().get(0));
+			}
+		} else if (playerTurnCounter == 2) {
+			
+			// +3 Cards
+			for (int i = 0; i < 3; i++) {
+				player2.drawCard(player2.getDeck().get(0));
+			}
+		} else if (playerTurnCounter == 3) {
+			
+			// +3 Cards
+			for (int i = 0; i < 3; i++) {
+				player3.drawCard(player3.getDeck().get(0));
+			}
+		} else if (playerTurnCounter == 4) {
+			
+			// +3 Cards
+			for (int i = 0; i < 3; i++) {
+				player4.drawCard(player4.getDeck().get(0));
 			}
 		}
 	}
